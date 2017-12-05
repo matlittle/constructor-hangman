757e313bc12205b40003621fae3d48f56cc5c95f
matlittle <matlittle727@gmail.com>
2017-12-04 20:28:43 -0600

    Moved random word function to its own file

--- hangman.js
+++ hangman.js
@@ -1,51 +1,13 @@
-
 const Prompt = require('prompt');
-const Request = require('request');
 
-const WordConstruct = require('./word.js');
-const LetterConstruct = require('./letter.js');
-
-
-getRandomWord();
+const WordConstruct = require('./word');
+const LetterConstruct = require('./letter');
+const RandomWord = require('./random');
 
 
-function getRandomWord() {
-    const endURL = "https://wordsapiv1.p.mashape.com/words/";
-    const randChar = String.fromCharCode(Math.floor(Math.random() * 27) + 97);
-    const randLen = Math.floor(Math.random() * 6) + 4;
-
-    const params = `?letterPattern=^${randChar}.{${randLen}}$&frequencyMin=3&limit=200`;
-    const reqURL = `${endURL}${params}`;
-
-    const headers = {
-        "X-Mashape-Key": "q4bESNa4dTmshLAQx5GNA4i2csGGp1AhjyUjsnMucLl8HYfsRp",
-        "Accept": "application/json"
-    }
-
-    const options = {
-        url: reqURL,
-        headers: headers
-    }
-
-    Request.get(options, handleWordReturn);
-}
-
-function handleWordReturn(err, res, body) {
-    if(err) throw err;
-
-    const words = JSON.parse(body).results.data;
-    const randNum = Math.floor(Math.random() * words.length);
-    const randWord = words[randNum];
-
-    if(randWord === undefined) {
-        getRandomWord();
-        return;
-    }
-
-    wordChosen(randWord);
-}
+RandomWord(wordChosen);
 
 function wordChosen(word) {
-
+    console.log(word);
 }
 
---
+++ random.js
@@ -0,0 +1,42 @@
+const Request = require('request');
+
+function RandomWord(callback) {
+    const options = getCallParams();
+
+    Request.get(options, function(err, res, body) {
+        if(err) throw err;
+        
+        const words = JSON.parse(body).results.data;
+        const randNum = Math.floor(Math.random() * words.length);
+        const randWord = words[randNum];
+    
+        if(randWord === undefined) {
+            RandomWord();
+            return;
+        }
+    
+        callback(randWord);
+    });
+}
+
+function getCallParams() {
+    const endURL = "https://wordsapiv1.p.mashape.com/words/";
+    const randChar = String.fromCharCode(Math.floor(Math.random() * 27) + 97);
+    const randLen = Math.floor(Math.random() * 6) + 4;
+
+    const params = `?letterPattern=^${randChar}.{${randLen}}$&frequencyMin=3&limit=200`;
+    const reqURL = `${endURL}${params}`;
+
+    const headers = {
+        "X-Mashape-Key": "q4bESNa4dTmshLAQx5GNA4i2csGGp1AhjyUjsnMucLl8HYfsRp",
+        "Accept": "application/json"
+    };
+
+    return {
+        url: reqURL,
+        headers: headers
+    };
+}
+
+
+module.exports = RandomWord;
